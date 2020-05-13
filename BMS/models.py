b8ef388f314730a74400355e11effc1795b77d2d
gemarkq <445454902@qq.com>
2020-05-14 00:24:16 +0800

    second commit, add models to database

---
+++ BMS/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+# Create your models here.
+
+class bms_admin(models.Model):
+    gh = models.CharField(primary_key=True, null=False, max_length=80)
+    name = models.CharField(max_length=80, null=True)
+    password = models.CharField(max_length=32, null=True)
+
+class readers(models.Model):
+    readerId = models.CharField(primary_key=True, null=False, max_length=80)
+    name = models.CharField(max_length=80, null=True)
+    phoneNumber = PhoneNumberField(null=True)
+    email = models.EmailField(null=True, unique=True)
+    balance = models.DecimalField(max_digits=8, decimal_places=3, default=100.000)
+
+class booklist(models.Model):
+    ISBN = models.CharField(primary_key=True, null=False, max_length=80)
+    bookName = models.CharField(null=True, max_length=80)
+    author = models.CharField(null=True, max_length=80)
+
+class books(models.Model):
+    STATUS = (
+        ('预约', '预约'),
+        ('借出', '借出'),
+        ('架上', '架上'),
+    )
+
+    ID = models.CharField(primary_key=True, null=False, max_length=80)
+    position = models.CharField(null=True, max_length=80)
+    status = models.CharField(default='架上', choices=STATUS, max_length=80)
+    ISBN = models.ForeignKey(booklist, on_delete=models.CASCADE)
+
+class borrow(models.Model):
+    STATUS = (
+        ('未归还', '未归还'),
+        ('已归还', '已归还'),
+    )
+
+    readerId = models.ForeignKey(readers, on_delete=models.CASCADE)
+    bookId = models.ForeignKey(books, on_delete=models.CASCADE)
+    borrowTime = models.DateField()
+    status = models.CharField(choices=STATUS, max_length=80)
+    returnTime = models.DateField()
+
+class reservation(models.Model):
+    reserveTime = models.DateField()
+    reserveLength = models.DecimalField(max_digits=3, decimal_places=0, default=5)
+    readerId = models.ForeignKey(readers, on_delete=models.CASCADE)
+    bookId = models.ForeignKey(books, on_delete=models.CASCADE)
+
+
